8f341dc96e561374c8fd35e12d0da4edd871b08d
Gustavo Pereira <gupersil04@gmail.com>
2022-08-18 20:15:14 -0300

    Programa que define três números em constantes e retorna true se pelo menos uma das três for par. Caso contrário, ele retorna false.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,15 +1,9 @@
-let nota = 78
+const a = 9
+const b = 5
+const c = 4
 
-if (nota >= 90 && nota <= 100) {
-    console.log("A")
-} else if (nota >= 80) {
-    console.log("B")
-} else if (nota >= 70) {
-    console.log("C")
-} else if (nota >= 60) {
-    console.log("D") 
-} else if (nota >= 50 && nota > 0) {
-    console.log("E")
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+    console.log(true)
 } else {
-    console.log("[ERRO] Nota inválida")
+    console.log(false)
 }
