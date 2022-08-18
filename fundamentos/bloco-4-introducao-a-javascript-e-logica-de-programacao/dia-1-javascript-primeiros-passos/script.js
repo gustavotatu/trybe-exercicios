d0451fc26ee2898ff92922900da94f80ea483a0b
Gustavo Pereira <gupersil04@gmail.com>
2022-08-18 20:42:19 -0300

    Programa que se inicia com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcula quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,9 +1,13 @@
-const a = 9
-const b = 5
-const c = 4
-
-if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
-    console.log(true)
+const valorCusto = 4
+const valorVenda = 10
+const milUnidadesProduzidas = valorCusto * 1000
+const milUnidadesVendidas = valorVenda * 1000
+const impostoSobreCusto = milUnidadesProduzidas / 100 * 20
+const valorCustoTotal = milUnidadesProduzidas + impostoSobreCusto
+const lucro = milUnidadesVendidas - valorCustoTotal
+ 
+if (valorCusto != 0 || valorVenda != 0) {
+    console.log(lucro)
 } else {
-    console.log(false)
+    console.log("[ERRO] Valor(es) de entrada inválido(s)")
 }
