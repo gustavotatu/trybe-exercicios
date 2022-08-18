c42c1ede91332d328b4a2a2a48a243b8a3ae7a49
Gustavo Pereira <gupersil04@gmail.com>
2022-08-18 19:39:53 -0300

    programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,30 +1,15 @@
-const entrada = "bispo";
-const peça = entrada.toLowerCase();
+let nota = 78
 
-switch (peça) {
-    case 'rei':
-        console.log("O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.");
-        break;
-
-    case 'rainha':
-        console.log("A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.");
-        break;
-
-    case 'bispo':
-        console.log("O bispo move-se ao longo da diagonal. Não pode pular outras peças.");
-        break;
-
-    case 'cavalo':
-        console.log("É a única peça que pode pular as outras. O movimento do cavalo é em forma de 'L', quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.");
-        break;
-    case 'torre':
-        console.log("A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.");
-        break;
-
-    case 'peão':
-        console.log("O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.");
-        break;
-
-    default:
-        console.log("[ERRO] Peça inválida")
+if (nota >= 90 && nota <= 100) {
+    console.log("A")
+} else if (nota >= 80) {
+    console.log("B")
+} else if (nota >= 70) {
+    console.log("C")
+} else if (nota >= 60) {
+    console.log("D") 
+} else if (nota >= 50 && nota > 0) {
+    console.log("E")
+} else {
+    console.log("[ERRO] Nota inválida")
 }
