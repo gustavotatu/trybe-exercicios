c206f0d3dd088a53eab973c9c6abef30f3fce3a9
Gustavo Pereira <gupersil04@gmail.com>
2022-08-18 18:42:06 -0300

    Programa que retorna o maior entre três números

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,8 +1,11 @@
 const a = 5
 const b = 3
+const c = 8
 
-if (a > b) {
+if (a > b && a > c) {
     console.log(a)
-} else if (b > a) {
+} else if (b > a && b > c) {
     console.log(b)
+} else if (c > a && c > b) {
+    console.log(c)
 }
