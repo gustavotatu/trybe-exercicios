1d211f28f127669f8ace5fbaa36a22326fe7a8b9
Gustavo Pereira <gupersil04@gmail.com>
2022-08-19 15:41:20 -0300

    Exercicio 3: calcule e imprima a média aritmética dos valores contidos no array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,8 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let valor = null
+let media = null
 
 for (let index = 0; index < numbers.length; index += 1) {
     valor = valor + numbers[index]
+    media = valor / numbers.length
 }
 
-console.log(valor)
+console.log(media)
