cec628c6185db870abec280a6491cd0868a19d74
Gustavo Pereira <gupersil04@gmail.com>
2022-08-18 23:56:02 -0300

    Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Este programa, dado um salário bruto, calcula o líquido a ser recebido.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,13 +1,46 @@
-const valorCusto = 4
-const valorVenda = 10
-const milUnidadesProduzidas = valorCusto * 1000
-const milUnidadesVendidas = valorVenda * 1000
-const impostoSobreCusto = milUnidadesProduzidas / 100 * 20
-const valorCustoTotal = milUnidadesProduzidas + impostoSobreCusto
-const lucro = milUnidadesVendidas - valorCustoTotal
- 
-if (valorCusto != 0 || valorVenda != 0) {
-    console.log(lucro)
-} else {
-    console.log("[ERRO] Valor(es) de entrada inválido(s)")
+const salarioBruto = 3000;
+let INSS = null
+let IR = null
+let salarioLiquido = null
+
+if (salarioBruto <= 1556.94) {
+    INSS = salarioBruto / 100 * 8
+    salarioLiquido = salarioBruto - INSS
+    console.log(salarioLiquido)
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    INSS = salarioBruto / 100 * 9
+    salarioLiquido = salarioBruto - INSS
+    if (salarioLiquido <= 1903.98) {
+        console.log(salarioLiquido)
+    } else if (salarioLiquido >= 1903.99 && salarioLiquido <= 2826.65) {
+        IR = salarioLiquido / 100 * 7.5 - 142.80
+        salarioLiquido = salarioLiquido - IR
+        console.log(salarioLiquido)
+    }
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    INSS = salarioBruto / 100 * 11
+    salarioLiquido = salarioBruto - INSS
+    if (salarioLiquido >= 1903.99 && salarioLiquido <= 2826.65) {
+        IR = salarioLiquido / 100 * 7.5 - 142.80
+        salarioLiquido = salarioLiquido - IR
+        console.log(salarioLiquido)
+    } else if (salarioLiquido >= 2826.66 && salarioLiquido <= 3751.05) {
+        IR = salarioLiquido / 100 * 15 - 354.80
+        salarioLiquido = salarioLiquido - IR
+        console.log(salarioLiquido)
+    } else if (salarioLiquido >= 3751.06 && salarioLiquido <= 4664.68) {
+        IR = salarioLiquido / 100 * 22.5 - 636.13
+        salarioLiquido = salarioLiquido - IR
+        console.log(salarioLiquido)
+    } else if (salarioLiquido > 4664.68) {
+        IR = salarioLiquido / 100 * 27.5 - 869.36
+        salarioLiquido = salarioLiquido - IR
+        console.log(salarioLiquido)
+    }
+} else if (salarioBruto > 5189.82) {
+    INSS = 570.88
+    salarioLiquido = salarioLiquido - INSS
+    IR = salarioLiquido / 100 * 27.5 - 869.36
+    salarioLiquido = salarioLiquido - IR
+    console.log(salarioLiquido)
 }
