996984e85530ccc88f8b6bc30bb360f2628710aa
Gustavo Pereira <gupersil04@gmail.com>
2022-08-19 17:11:22 -0300

    Exercício 9: Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,7 +1,12 @@
 let numbers = []
+let resultado = []
 
 for (let index = 1; index < 26; index += 1) {
     numbers.push(index)
 }
 
-console.log(numbers)
+for (let index = 0; index < numbers.length; index += 1) {
+    resultado.push(numbers[index] / 2)
+}
+
+console.log(resultado)
