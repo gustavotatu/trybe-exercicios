7a524ef62fb7446fef6fb7066f754d7f574cf14f
Gustavo Pereira <gupersil04@gmail.com>
2022-08-22 18:26:29 -0300

    Exercício 4: escreva um algoritmo que retorne o maior número primo entre 2 e 50.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,49 +1,18 @@
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let maior = ''
-let menor = ''
-for (index = 0; index < array.length; index += 1) {
-    for (index2 = 0; index2 < array.length;){
-        if (array[index] != array[index2]) {
-            if (array[index].length > array[index2].length) {
-                if (index2 == array.length - 1) {
-                    maior += array[index]
-                    index2 = array.length
-                    index = array.length
-                } else {
-                    index2 += 1
-                }
-            } else {
-                index2 = array.length
-            }
+let resultado = null
+
+for (index = 3; index < 50; index += 2) {
+    for (index2 = 2; index2 < index;) {
+        if (index == 3) {
+            index2 = index
+        } else if (index % index2 == 0) {
+            index2 = index
+        } else if (index2 == index - 1) {
+            resultado = index
+            index2 += 1
         } else {
             index2 += 1
         }
     }
 }
 
-for (index = 0; index < array.length; index += 1) {
-    for (index2 = 0; index2 < array.length;){
-        if (array[index] != array[index2]) {
-            if (array[index].length < array[index2].length) {
-                if (index2 == array.length - 1) {
-                    menor += array[index]
-                    index2 = array.length
-                    index = array.length
-                } else {
-                    index2 += 1
-                }
-            } else {
-                index2 = array.length
-            }
-        } else  if (index2 == array.length - 1) {
-            menor += array[index]
-            index2 = array.length
-            index = array.length
-        } else {
-            index2 += 1
-        }
-    }
-}
-
-console.log(maior)
-console.log(menor)
+console.log(resultado)
