5aee51393a062a654c0f016b9684941039e87057
Gustavo Pereira <gupersil04@gmail.com>
2022-08-22 15:59:15 -0300

    Exercício 3: Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,7 +1,49 @@
-let word = 'tryber';
-let resultado = '';
-for (index = word.length - 1; index >= 0; index -= 1) {
-    resultado += word[index]
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = ''
+let menor = ''
+for (index = 0; index < array.length; index += 1) {
+    for (index2 = 0; index2 < array.length;){
+        if (array[index] != array[index2]) {
+            if (array[index].length > array[index2].length) {
+                if (index2 == array.length - 1) {
+                    maior += array[index]
+                    index2 = array.length
+                    index = array.length
+                } else {
+                    index2 += 1
+                }
+            } else {
+                index2 = array.length
+            }
+        } else {
+            index2 += 1
+        }
+    }
 }
 
-console.log(resultado)
+for (index = 0; index < array.length; index += 1) {
+    for (index2 = 0; index2 < array.length;){
+        if (array[index] != array[index2]) {
+            if (array[index].length < array[index2].length) {
+                if (index2 == array.length - 1) {
+                    menor += array[index]
+                    index2 = array.length
+                    index = array.length
+                } else {
+                    index2 += 1
+                }
+            } else {
+                index2 = array.length
+            }
+        } else  if (index2 == array.length - 1) {
+            menor += array[index]
+            index2 = array.length
+            index = array.length
+        } else {
+            index2 += 1
+        }
+    }
+}
+
+console.log(maior)
+console.log(menor)
