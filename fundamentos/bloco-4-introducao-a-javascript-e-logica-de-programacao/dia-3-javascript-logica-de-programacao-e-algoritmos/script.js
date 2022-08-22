7d4c07f6e85b3f65b9dbec96af5d56bc624029be
Gustavo Pereira <gupersil04@gmail.com>
2022-08-22 15:27:02 -0300

    Exercício 2: desenvolva um algoritmo que é capaz de inverter uma palavra.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,6 +1,7 @@
-let resultado = 10
-for (let index = 9; index > 0; index -= 1) {
-    resultado *= index
+let word = 'tryber';
+let resultado = '';
+for (index = word.length - 1; index >= 0; index -= 1) {
+    resultado += word[index]
 }
 
 console.log(resultado)
