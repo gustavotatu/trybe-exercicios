e6e03ec2f2c6d4f94030e45d463ddda441c89c22
Gustavo Pereira <gupersil04@gmail.com>
2022-08-22 15:10:51 -0300

    Alteração

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,12 +1,21 @@
-let numbers = []
-let resultado = []
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let crescente = []
+let index = 0
 
-for (let index = 1; index < 26; index += 1) {
-    numbers.push(index)
-}
+for (let index2 = 0; crescente.length <= 6; index2 += 1) {
+    if (numbers[index] != numbers[index2] && numbers[index] < numbers[index2]) {
+        if (index2 == numbers.length - 1) {
+            crescente.push(numbers.splice(index, 1))
+            index2 = -1
+            index = 0
+        }
+    } else if (numbers[index] > numbers[index2]) {
+        index += 1
+        index2 = -1
+    } else if (numbers.length == 1) {
+        crescente.push(numbers.splice(index, 1))
+    }
+}    
 
-for (let index = 0; index < numbers.length; index += 1) {
-    resultado.push(numbers[index] / 2)
-}
 
-console.log(resultado)
+console.log(crescente)
