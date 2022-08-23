6b3245f9cb2185a191ad8bb5abd4d913ea3d0a41
Gustavo Pereira <gupersil04@gmail.com>
2022-08-23 18:31:28 -0300

    Exercício 5: defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/script.js
@@ -6,6 +6,19 @@ let info = {
 
  info['recorrente'] = 'Sim'
  
- for (let index in info) {
-    console.log(info[index])
- }
+ let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+  };
+
+info2['recorrente'] = 'Sim'
+
+for (let index in info) {
+        if (info[index] != info2[index]) {
+        console.log(info[index] + ' e ' + info2[index])
+    } else {
+        console.log('Ambos são ' + index + 's') 
+    }
+}
+
