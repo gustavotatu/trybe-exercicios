3fd47343932dde93f3e74c949c56bfa74b21beb6
Gustavo Pereira <gupersil04@gmail.com>
2022-09-22 19:33:39 -0300

    Exercício 2: Sortedor de loteria

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,15 +1,13 @@
-const info = (nome, sobrenome) => ({
-    Nome: `${nome} ${sobrenome}`,
-    Email: `${nome}_${sobrenome}@trybe.com`, 
-})
+const sorteio = () => {
+    const numeroSorteado = Math.floor(Math.random() * 5 + 1);
+    return numeroSorteado;
+};
 
-const newEmployees = () => {
-    const employees = {
-      id1: info('Pedro', 'Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: info('Luiza', 'Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: info('Carla', 'Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+const result = (numberBet, callback) => {
+    if (numberBet === callback()) {
+        return 'Parabéns, você ganhou!';
     }
-    return employees;
-  };
-  
-  console.log(newEmployees());
+    return 'Não foi dessa vez';
+}
+
+console.log(result(2, sorteio));
