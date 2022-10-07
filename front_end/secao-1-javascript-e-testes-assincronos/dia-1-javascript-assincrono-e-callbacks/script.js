56634fc19991930088b9db65397b0783c9aff30a
Gustavo Pereira <gupersil04@gmail.com>
2022-10-07 00:36:57 -0300

    Exercício 2

--- front_end/secao-1-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front_end/secao-1-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -1,12 +1,11 @@
-const getPlanet = () => {
-    const mars = {
-      name: 'Mars',
-      distanceFromSun: {
-        value: 227900000,
-        measurementUnit: 'kilometers',
-      },
-    };
-    console.log('Returned planet: ', mars);
-  };
-  
-  setTimeout(() => getPlanet(), 4000); // Imprime Marte depois de 4 segundos
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = ((temperature) => `A temperatura de Marte é: ${getMarsTemperature()} graus celsius`);
+
+setTimeout(() => console.log(sendMarsTemperature()), messageDelay()); // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
